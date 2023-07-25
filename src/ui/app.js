f9cfb8c8ee4be0b2ab4db4f82a5089f39c7d9ab1
florencia.vasiliauskas <florvasiliauskas@gmail.com>
2023-07-25 15:27:21 -0300

    Sections in DOM

--- src/ui/app.js
+++ src/ui/app.js
@@ -4,25 +4,34 @@ function uploadFile() {
     const selectedFile = fileInput.files[0];
 
     if (selectedFile) {
-      const formData = new FormData();
-      formData.append("jsonFile", selectedFile);
+        const formData = new FormData();
+        formData.append("jsonFile", selectedFile);
+        console.log(selectedFile)
 
-      fetch("http://127.0.0.1:5000/", {
-        method: "POST",
-        body: formData
-      })
-      .then(response => response.json())
-      .then(data => {
-        if (data.message) {
-          alert(data.message);
-        } else {
-          alert(data.error);
-        }
-      })
-      .catch(error => {
-        console.error("Error:", error);
-      });
+        fetch(selectedFile.path)
+            .then(response => response.json())
+            .then(data => {
+                if (data.message) {
+                    alert(data.message);
+                } else {
+                    alert(data.error);
+                }
+                printSections(data)
+            })
+            .catch(error => {
+                console.error("Error:", error);
+            });
     } else {
-      alert("Please select a JSON file to upload.");
+        alert("Please select a JSON file to upload.");
     }
-  }
+}
+
+function printSections(data) {
+    const menuSections = data.MenuSections;
+    const sections = document.getElementById('ulSecciones');
+
+    menuSections.forEach(menuSection => {
+        const name = menuSection.Name;
+        sections.insertAdjacentHTML('beforeend', `<li class="liSecciones">${name}</li>`);
+    });
+}
