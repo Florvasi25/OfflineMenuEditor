26a5bcafbe852bfe3f6adde1851339c90e68c52a
florencia.vasiliauskas <florvasiliauskas@gmail.com>
2023-08-07 12:36:17 -0300

    localStorage modifications

--- src/ui/app.js
+++ src/ui/app.js
@@ -1,6 +1,7 @@
 import { saveToFile } from './file.js';
 
-let jsonData = {};
+// let jsonData = {};
+let jsonData = JSON.parse(localStorage.getItem("jsonData")) ?? {};
 
 document.getElementById('jsonFileInput').addEventListener('change', function (event) {
     const file = event.target.files[0];
@@ -23,16 +24,29 @@ function generateHTML(data) {
         const sectionDiv = document.createElement('div');
         sectionDiv.classList.add('menu');
         sectionDiv.id = menuSection.MenuSectionId;
+        
+        if (!menuSection.IsAvailable) {
+            sectionDiv.classList.add('unavailable');
+        }
+        
         sectionDiv.innerHTML = `
             <p contenteditable="true" class="name">${menuSection.Name}</p>
             <p contenteditable="true" class="IsAvailable">${menuSection.IsAvailable}</p>
         `;
+        
+        sectionDiv.querySelectorAll('[contenteditable="true"]').forEach(editable => {
+            editable.addEventListener('input', function () {
+                updateDataFromUI();
+            });
+        });
+
         outputContainer.appendChild(sectionDiv);
     });
+
     localStorage.setItem("jsonData", JSON.stringify(jsonData.MenuSections));
 }
 
-document.getElementById('saveButton').addEventListener('click', function () {
+function updateDataFromUI() {
     const menuSections = document.getElementsByClassName('menu');
 
     Array.from(menuSections).forEach(section => {
@@ -42,10 +56,17 @@ document.getElementById('saveButton').addEventListener('click', function () {
         const name = section.getElementsByClassName('name')[0].textContent;
         const isAvailable = section.getElementsByClassName('IsAvailable')[0].textContent;
 
-        jsonData.MenuSections[sectionIndex].Name = name
-        jsonData.MenuSections[sectionIndex].IsAvailable = isAvailable
+        jsonData.MenuSections[sectionIndex].Name = name;
+        jsonData.MenuSections[sectionIndex].IsAvailable = isAvailable;
     });
 
+    localStorage.setItem("jsonData", JSON.stringify(jsonData.MenuSections));
+}
+
+document.getElementById('saveButton').addEventListener('click', function () {
+    updateDataFromUI();
+    
     console.log(jsonData);
-    saveToFile(jsonData)
+    saveToFile(jsonData);
 });
+
