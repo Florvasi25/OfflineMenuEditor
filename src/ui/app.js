8e6e35aa6dd75c02580c445de76a007c5611a87e
florencia.vasiliauskas <florvasiliauskas@gmail.com>
2023-08-08 11:07:38 -0300

    Create New Section

--- src/ui/app.js
+++ src/ui/app.js
@@ -21,36 +21,41 @@ function generateHTML(data) {
     outputContainer.innerHTML = '';
 
     data.MenuSections.forEach(menuSection => {
-        const sectionDiv = document.createElement('div');
-        sectionDiv.classList.add('mainSection');
-        sectionDiv.id = menuSection.MenuSectionId;
-
-        if (!menuSection.IsAvailable) {
-            sectionDiv.classList.add('unavailable');
-        }
-
-        const nameParagraph = document.createElement('p');
-        nameParagraph.contentEditable = true;
-        nameParagraph.classList.add('name');
-        nameParagraph.textContent = menuSection.Name;
-        sectionDiv.appendChild(nameParagraph);
-
-        nameParagraph.addEventListener('input', function () {
-            updateDataFromUI();
-        });
+        let sectionDiv = createSection(menuSection)
 
         outputContainer.appendChild(sectionDiv);
     });
 }
 
+function createSection(menuSection){
+    const sectionDiv = document.createElement('div');
+    sectionDiv.classList.add('sectionContainer');
+    sectionDiv.id = menuSection.MenuSectionId;
+
+    if (!menuSection.IsAvailable) {
+        sectionDiv.classList.add('unavailable');
+    }
+
+    const sectionName = document.createElement('p');
+    sectionName.contentEditable = true;
+    sectionName.classList.add('sectionName');
+    sectionName.textContent = menuSection.Name;
+    sectionDiv.appendChild(sectionName);
+
+    sectionName.addEventListener('input', updateDataFromUI);
+
+    return sectionDiv
+    // outputContainer.appendChild(sectionDiv);
+}
+
 function updateDataFromUI() {
-    const menuSections = document.getElementsByClassName('mainSection');
+    const menuSections = document.getElementsByClassName('sectionContainer');
 
     Array.from(menuSections).forEach(section => {
         const sectionId = section.id;
         const sectionIndex = jsonData.MenuSections.findIndex(sectionIndex => sectionIndex.MenuSectionId == sectionId);
 
-        const name = section.getElementsByClassName('name')[0].textContent;
+        const name = section.getElementsByClassName('sectionName')[0].textContent;
 
         jsonData.MenuSections[sectionIndex].Name = name;
     });
@@ -65,6 +70,37 @@ document.getElementById('saveButton').addEventListener('click', function () {
     saveToFile(jsonData);
 });
 
-// Call generateHTML when the page loads
+document.getElementById('addSectionButton').addEventListener('click', () => {
+
+    const emptySectionJson = {
+            MenuSectionId: 1,
+            Name: "EMPTY",
+            Description: null,
+            DisplayOrder: 0,
+            MenuItems: [],
+            PublicId: "167876d3-b8ae-467e-ab8e-8a0dfda2b08e",
+            IsDeleted: false,
+            IsAvailable: false,
+            IsHiddenFromUsers: true,
+            ImageName: null,
+            ImageUrl: null,
+            CellAspectRatio: 0,
+            CellLayoutType: 0,
+            MenuSectionAvailability: {
+                MenuSectionId: 129293,
+                AvailableTimes: null,
+                AvailabilityMode: 0
+            },
+            ConcessionStoreId: null,
+            MenuSectionMetadata: []
+        };
+
+    let sectionDiv = createSection(emptySectionJson)
+    document.getElementById('outputContainer').appendChild(sectionDiv);
+    jsonData.MenuSections.push(emptySectionJson)
+    localStorage.setItem("jsonData", JSON.stringify(jsonData));
+
+});
+
 generateHTML(jsonData);
 
