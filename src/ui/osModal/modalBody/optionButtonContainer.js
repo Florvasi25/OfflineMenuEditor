8df0b6689272fb9b4955797c3203b84c63762735
florencia.vasiliauskas <florvasiliauskas@gmail.com>
2023-10-05 11:49:15 -0300

    optionVisibility

--- src/ui/osModal/modalBody/optionButtonContainer.js
+++ src/ui/osModal/modalBody/optionButtonContainer.js
@@ -6,9 +6,15 @@ import {
     optionDeleteButton
 } from './optionDelete.js'
 
+import {
+    optionVisibilityButton
+} from './optionVisibility.js'
+
 function createOptionButtonsCell(optionRow, optionId, sectionId, itemId, osId, optionsBodyContainer, menuOption) {
     const optionButtonsCell = document.createElement('div');
     optionButtonsCell.classList = "optionButtonsCell"
+
+    optionVisibilityButton(optionButtonsCell, optionRow, menuOption, sectionId, itemId, osId)
 
     optionDeleteButton(optionButtonsCell, optionRow, sectionId, itemId, osId, optionsBodyContainer)
 
---
+++ src/ui/osModal/modalBody/optionVisibility.js
@@ -0,0 +1,52 @@
+import {
+    updateSectionLocalStorage,
+    getOptionIndex,
+    jsonData,
+} from '../../context.js'
+
+function optionVisibilityButton(optionButtonsCell, optionRow, menuOption, sectionId, itemId, osId) {
+    const visibilityButton = document.createElement('button');
+    visibilityButton.classList.add('sectionButton')
+    visibilityButton.classList.add('visibilityButton')
+    visibilityButton.addEventListener('click', () => {
+        SectionAvailability(optionRow, sectionId, itemId, osId, optionRow.id);
+    });
+    optionButtonsCell.appendChild(visibilityButton);
+    const visibilityButtonImg = document.createElement('img')
+    visibilityButtonImg.classList.add('sectionButtonImg')
+    visibilityButtonImg.src = '../../assets/visibilityIcon.svg'
+    visibilityButton.appendChild(visibilityButtonImg)
+
+    //Unavailable Sections - Gray
+    if (!menuOption.IsAvailable) {
+        optionRow.classList.add('unavailable');
+        visibilityButton.classList.add('hiddenSection')
+    }
+
+    //Changes the color of the Visibility Button according to its availability
+    visibilityButton.addEventListener('click', () => {
+        if (!menuOption.IsAvailable) {
+            visibilityButton.classList.add('hiddenSection')
+        } else {
+            visibilityButton.classList.remove('hiddenSection')
+        }
+    })
+}
+
+//Section Availability
+function SectionAvailability(optionRow, sectionId, itemId, osId, optionId) {
+
+    const {sectionIndex, itemIndex, osIndex, optionIndex} = getOptionIndex(sectionId, itemId, osId, optionId);
+    if (itemIndex !== -1) {
+        const isAvailableNew = !jsonData.MenuSections[sectionIndex].MenuItems[itemIndex].MenuItemOptionSets[osIndex].MenuItemOptionSetItems[optionIndex].IsAvailable
+        jsonData.MenuSections[sectionIndex].MenuItems[itemIndex].MenuItemOptionSets[osIndex].MenuItemOptionSetItems[optionIndex].IsAvailable = isAvailableNew
+
+        optionRow.classList.toggle('unavailable', !isAvailableNew);
+
+        updateSectionLocalStorage()
+    }
+}
+
+export {
+    optionVisibilityButton,
+}
