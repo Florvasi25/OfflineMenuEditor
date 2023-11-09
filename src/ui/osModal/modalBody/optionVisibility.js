7a2b070b5e19d82dace9f6fe04bec1158c900a75
florencia.vasiliauskas <florvasiliauskas@gmail.com>
2023-11-09 20:30:01 -0300

    Fix option visibility bug in modalBody

--- src/ui/osModal/modalBody/optionVisibility.js
+++ src/ui/osModal/modalBody/optionVisibility.js
@@ -39,38 +39,39 @@ function optionVisibilityButton(optionButtonsCell, optionRow, menuOption, menuOs
 //Section Availability
 function SectionAvailability(optionRow, menuOs, menuOption) {
     const optionToHide = optionRow.id;
+    const oldGroupOsId = menuOs.groupOsId
 
     if (optionToHide) {
-        if (groupedOs[menuOs.groupOsId]) {
-            const optionObject = groupedOs[menuOs.groupOsId][0].MenuItemOptionSetItems.find(option => option.groupOptionId == optionToHide)
+        if (groupedOs[oldGroupOsId]) {
+            const optionObject = groupedOs[oldGroupOsId][0].MenuItemOptionSetItems.find(option => option.groupOptionId == optionToHide)
             const isAvailableNew = !optionObject.IsAvailable
-            groupedOs[menuOs.groupOsId].forEach(os => {
+            groupedOs[oldGroupOsId].forEach(os => {
                 const option = os.MenuItemOptionSetItems.find(option => option.groupOptionId == optionToHide)
                 option.IsAvailable = isAvailableNew
                 optionRow.classList.toggle('unavailable', !isAvailableNew);
-            }) 
+            })
             groupOptionSets()
             updateLocalStorage()
-        } else if (itemlessOs[menuOs.groupOsId]) {
-            const option = itemlessOs[menuOs.groupOsId].MenuItemOptionSetItems.find(option => option.groupOptionId == optionToHide)
+        } else if (itemlessOs[oldGroupOsId]) {
+            const option = itemlessOs[oldGroupOsId].MenuItemOptionSetItems.find(option => option.groupOptionId == optionToHide)
             const isAvailableNew = !option.IsAvailable
             option.IsAvailable = isAvailableNew
             optionRow.classList.toggle('unavailable', !isAvailableNew);
-            addItemlessOs(itemlessOs[menuOs.groupOsId])
-            deleteItemlessOs(menuOs.groupOsId)
+            addItemlessOs(itemlessOs[oldGroupOsId])
+            deleteItemlessOs(oldGroupOsId)
         }
-            
+
             // const optionContainerPreviewArray = Array.from(document.getElementsByClassName('optionContainer'));
 
             // const optionContainerPreview = optionContainerPreviewArray.filter((element) => {
             //   const groupOsId = element.getAttribute('groupOsId');
             //   return groupOsId === menuOs.groupOsId;
             // });
-            
+
             // if (optionContainerPreview) {
             //     optionContainerPreview.forEach((osRowOptionContainerPreview) => {
             //         const osRowOptionPreviewArray = Array.from(osRowOptionContainerPreview.getElementsByClassName('osRowOption'));
-                    
+
             //         osRowOptionPreviewArray.forEach(osRowOptionPreview => {
             //             if (osRowOptionPreview.id === menuOption.groupOptionId) {
             //                 osRowOptionPreview.classList.toggle('unavailable', !isAvailableNew)
