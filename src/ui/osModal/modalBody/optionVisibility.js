93c45f7ed23a3361e03734f179dfe29241f837fd
florencia.vasiliauskas <florvasiliauskas@gmail.com>
2023-11-09 23:40:04 -0300

    Refactor optionVisibilityButton function to improve readability and add missing semicolon

--- src/ui/osModal/modalBody/optionVisibility.js
+++ src/ui/osModal/modalBody/optionVisibility.js
@@ -6,67 +6,73 @@ import {
     addItemlessOs,
     deleteItemlessOs,
     updateOsDomIds,
-    updateOptionDomIds
-} from '../../context.js'
+    updateOptionDomIds,
+} from "../../context.js";
 
-function optionVisibilityButton(optionButtonsCell, optionRow, menuOption, menuOs) {
-    const visibilityButton = document.createElement('button');
-    visibilityButton.classList.add('sectionButton')
-    visibilityButton.classList.add('visibilityButton')
-    visibilityButton.addEventListener('click', () => {
+function optionVisibilityButton(
+    optionButtonsCell,
+    optionRow,
+    menuOption,
+    menuOs
+) {
+    const visibilityButton = document.createElement("button");
+    visibilityButton.classList.add("sectionButton");
+    visibilityButton.classList.add("visibilityButton");
+    visibilityButton.addEventListener("click", () => {
         SectionAvailability(optionRow, menuOs, menuOption);
     });
     optionButtonsCell.appendChild(visibilityButton);
-    const visibilityButtonImg = document.createElement('img')
-    visibilityButtonImg.classList.add('sectionButtonImg')
-    visibilityButtonImg.src = '../../assets/visibilityIcon.svg'
-    visibilityButton.appendChild(visibilityButtonImg)
+    const visibilityButtonImg = document.createElement("img");
+    visibilityButtonImg.classList.add("sectionButtonImg");
+    visibilityButtonImg.src = "../../assets/visibilityIcon.svg";
+    visibilityButton.appendChild(visibilityButtonImg);
 
     //Unavailable Sections - Gray
     if (!menuOption.IsAvailable) {
-        optionRow.classList.add('unavailable');
-        visibilityButton.classList.add('hiddenSection')
+        optionRow.classList.add("unavailable");
+        visibilityButton.classList.add("hiddenSection");
     }
 
     //Changes the color of the Visibility Button according to its availability
-    visibilityButton.addEventListener('click', () => {
+    visibilityButton.addEventListener("click", () => {
         if (!menuOption.IsAvailable) {
-            visibilityButton.classList.add('hiddenSection')
+            visibilityButton.classList.add("hiddenSection");
         } else {
-            visibilityButton.classList.remove('hiddenSection')
+            visibilityButton.classList.remove("hiddenSection");
         }
-    })
+    });
 }
 
 //Section Availability
 function SectionAvailability(optionRow, menuOs, menuOption) {
     const optionToHide = optionRow.id;
-    const oldGroupOsId = menuOs.groupOsId
-    const oldGroupOptionId = menuOption.groupOptionId
+    const oldGroupOsId = menuOs.groupOsId;
+    const oldGroupOptionId = menuOption.groupOptionId;
 
     if (optionToHide) {
         if (groupedOs[oldGroupOsId]) {
-            const optionObject = groupedOs[oldGroupOsId][0].MenuItemOptionSetItems.find(option => option.groupOptionId == optionToHide)
-            const isAvailableNew = !optionObject.IsAvailable
-            groupedOs[oldGroupOsId].forEach(os => {
-                const option = os.MenuItemOptionSetItems.find(option => option.groupOptionId == optionToHide)
-                option.IsAvailable = isAvailableNew
-                optionRow.classList.toggle('unavailable', !isAvailableNew);
-            })            
-            groupOptionSets()
-            updateLocalStorage()
+            groupedOs[oldGroupOsId].forEach((os) => {
+                const option = os.MenuItemOptionSetItems.find(
+                    (option) => option.groupOptionId == optionToHide
+                );
+                optionRow.classList.toggle("unavailable", option.IsAvailable);
+                option.IsAvailable = !option.IsAvailable;
+            });
+            groupOptionSets();
+            updateLocalStorage();
 
-            updateOptionDomIds(menuOption, oldGroupOptionId)
-            updateOsDomIds(menuOs, oldGroupOsId)
+            updateOptionDomIds(menuOption, oldGroupOptionId);
+            updateOsDomIds(menuOs, oldGroupOsId);
         } else if (itemlessOs[oldGroupOsId]) {
-            const option = itemlessOs[oldGroupOsId].MenuItemOptionSetItems.find(option => option.groupOptionId == optionToHide)
-            const isAvailableNew = !option.IsAvailable
-            option.IsAvailable = isAvailableNew
-            optionRow.classList.toggle('unavailable', !isAvailableNew);
-            addItemlessOs(itemlessOs[oldGroupOsId])
-            deleteItemlessOs(oldGroupOsId)
+            const option = itemlessOs[oldGroupOsId].MenuItemOptionSetItems.find(
+                (option) => option.groupOptionId == optionToHide
+            );
+            optionRow.classList.toggle("unavailable", option.IsAvailable);
+            option.IsAvailable = !option.IsAvailable;
+            addItemlessOs(itemlessOs[oldGroupOsId]);
+            deleteItemlessOs(oldGroupOsId);
         }
     }
 }
 
-export { optionVisibilityButton }
+export { optionVisibilityButton };
