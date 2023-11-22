edea405ecb267dc7a9adb705cd2e7977e4debbbe
florencia.vasiliauskas <florvasiliauskas@gmail.com>
2023-11-22 09:40:02 -0300

    dropdownContent in osExpandList closes when clickling outside

--- src/ui/osModal/modalNav/osExpandList.js
+++ src/ui/osModal/modalNav/osExpandList.js
@@ -27,6 +27,20 @@ function osExpandListButton(osBtnsCell) {
     
     expandButtonContainer.appendChild(expandListButton)
     expandButtonContainer.appendChild(dropdownContent);
+
+    // Add a click event listener to the document
+    document.addEventListener('click', function(event) {
+        const dropdownContent = document.getElementById('myDropdown');
+        const expandListButton = document.querySelector('.expandListButton');
+        
+        const isDropdownShown = dropdownContent.classList.contains('showOsList');
+        const isClickedInsideDropdown = dropdownContent.contains(event.target);
+        const isClickedOnButton = expandListButton.contains(event.target);
+
+        if (isDropdownShown && !isClickedInsideDropdown && !isClickedOnButton) {
+            toggleDropdown();
+        }
+    });
     
     osBtnsCell.appendChild(expandButtonContainer);
 }
