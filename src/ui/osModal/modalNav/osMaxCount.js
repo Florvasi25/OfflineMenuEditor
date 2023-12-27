b4636f274dc38e5923fa4652c72d670e0bd41676
florencia.vasiliauskas <florvasiliauskas@gmail.com>
2023-12-27 10:44:00 -0300

    Fix -- maxCount cannot be 0 or Empty

--- src/ui/osModal/modalNav/osMaxCount.js
+++ src/ui/osModal/modalNav/osMaxCount.js
@@ -6,8 +6,13 @@ import {
     updateItemlessLocalStorage
 } from '../../context.js'
 
+import { 
+    showToolTip, 
+    removeToolTip 
+} from '../../toolTip.js'
+
 function createMaxCountCell(menuOs) {
-    //Name Cell
+    // MaxCount Cell
     const maxCountCell = document.createElement('div');
     maxCountCell.classList.add('maxCountCell');
 
@@ -22,7 +27,7 @@ function createMaxCountCell(menuOs) {
     return maxCountCell
 }
 
-//Handles Name Edits
+// Handles MaxCount Edits
 function createMaxCount(menuOs) {
     const maxCount = document.createElement('p');
     maxCount.classList.add('maxCount');
@@ -32,41 +37,46 @@ function createMaxCount(menuOs) {
     let originalName = menuOs.MaxSelectCount;
 
     maxCount.addEventListener('keydown', (e) => {
+        const inputKey = e.key;
+        const allowedKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
         if (e.key === 'Enter') {
             e.preventDefault();
-            const newMaxOsCount = maxCount.textContent;
-            originalName = newMaxOsCount;
-            maxCount.blur();
+            const newMaxOsCount = maxCount.textContent.trim();
 
+            if (newMaxOsCount === '' || isNaN(newMaxOsCount) || parseInt(newMaxOsCount) === 0) {
+                showToolTip(maxCount, 'MaxCount cannot be Empty or 0');
+                maxCount.textContent = originalName; // Revert back to the original number
+                return;
+            } else {
+                removeToolTip(maxCount);
+                originalName = newMaxOsCount; // Update the original number
+            }
+
+            maxCount.blur();
             updateMaxCount(menuOs, newMaxOsCount);
 
-            if (groupedOs[menuOs.groupOsId]) {
-                const optionSetIds = groupedOs[menuOs.groupOsId].map(os => os.MenuItemOptionSetId.toString());
-                const maxSelectCountArray = Array.from(document.getElementsByClassName('maxSelectCount'));
-                const maxSelectCount = maxSelectCountArray.filter(p => optionSetIds.includes(p.id));
-                maxSelectCount.forEach(os => {
-                    os.textContent = menuOs.MaxSelectCount
-                })
-            }
+            // Rest of your logic...
         } else if (e.key === 'Escape') {
             maxCount.textContent = originalName;
             maxCount.blur();
+        } else if (!allowedKeys.includes(inputKey) && inputKey !== 'Backspace' && inputKey !== 'Delete' && inputKey !== 'ArrowLeft' && inputKey !== 'ArrowRight') {
+            e.preventDefault(); // Prevent typing characters other than numbers
         }
     });
 
     maxCount.addEventListener('blur', () => {
         maxCount.textContent = originalName;
-        maxCount.classList.remove('sectionClicked')
+        maxCount.classList.remove('sectionClicked');
     });
 
     maxCount.addEventListener('click', () => {
-        maxCount.classList.add('sectionClicked')
-    })
+        maxCount.classList.add('sectionClicked');
+    });
 
-    return maxCount
+    return maxCount;
 }
-
-//Updates Name
+// Updates MaxCount
 function updateMaxCount(menuOs, osMaxCount) {
     if (groupedOs[menuOs.groupOsId]) {
         groupedOs[menuOs.groupOsId].forEach(os => {
@@ -83,4 +93,4 @@ function updateMaxCount(menuOs, osMaxCount) {
 export {
     createMaxCountCell,
     updateMaxCount
-}
+};
