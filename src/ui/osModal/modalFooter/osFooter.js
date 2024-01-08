43d76134e7678e87255741dabfb0311e96261c2d
florencia.vasiliauskas <florvasiliauskas@gmail.com>
2024-01-08 15:22:19 -0300

    OS is added to the first item

--- src/ui/osModal/modalFooter/osFooter.js
+++ src/ui/osModal/modalFooter/osFooter.js
@@ -1,4 +1,16 @@
-import { jsonData } from "../../context.js";
+import { 
+    jsonData,
+    deleteItemlessOs,
+    getLocalStorageOptionSetIDs,
+    getUniqueRandomInt,
+    getLocalStorageOptionSetItemsIDs,
+    groupOptionSets,
+    updateLocalStorage
+ } from "../../context.js";
+
+ import {
+    createOsRow
+ } from '../../optionSet/osHeaderContainer.js'
 
 function createOsModalFooter(menuOs) {
     const osModalFooter = document.createElement('div');
@@ -112,29 +124,93 @@ function createShowOs(menuOs) {
 
         const sectionHeader = document.createElement('p');
         sectionHeader.className = 'sectionHeader';
-        sectionHeader.textContent = `${sectionName} (${sectionsWithFilteredItems[sectionName]?.count || 0}/${section.MenuItems.length})`;
-        showOs.appendChild(sectionHeader);
 
-        const itemList = document.createElement('ul');
-        itemList.className = 'itemList';
+        if (section.MenuItems.length !== 0) {
+            sectionHeader.textContent = `${sectionName} (${sectionsWithFilteredItems[sectionName]?.count || 0}/${section.MenuItems.length})`;
+            showOs.appendChild(sectionHeader);
+        }
+
+        const itemListContainer = document.createElement('ul');
+        itemListContainer.className = 'itemListContainer';
 
         allItems.forEach(item => {
             const listedItem = document.createElement('li');
             listedItem.className = 'listedItem';
-            listedItem.textContent = item.Name;
 
+            const itemName = document.createElement('p')
+            itemName.textContent = item.Name;
+   
+            listedItem.appendChild(itemName);
+            
             // Highlight the listedItem if it belongs to the filtered section
             if (sectionsWithFilteredItems[sectionName] && sectionsWithFilteredItems[sectionName].items.includes(item)) {
                 listedItem.style.backgroundColor = '#8ef274';
+            } else {
+                const addButton = createAddButton(menuOs)
+                listedItem.appendChild(addButton);
             }
 
-            itemList.appendChild(listedItem);
+            itemListContainer.appendChild(listedItem);
         });
 
-        showOs.appendChild(itemList);
+        showOs.appendChild(itemListContainer);
     });
 
     return showOs;
+}
+
+function createAddButton(menuOs) {
+    const addBtn = document.createElement('button')
+    addBtn.className = 'addOrDeleteBtn'
+    addBtn.textContent = '+'
+
+    const foundItem = jsonData.MenuSections.flatMap(i => i.MenuItems).find(i => i.MenuItemId == menuOs.MenuItemId)
+
+    addBtn.addEventListener('click', () => {
+
+        const newOs = JSON.parse(JSON.stringify(menuOs));
+
+        deleteItemlessOs(newOs)
+
+        newOs.MenuItemId = foundItem.MenuItemId
+
+        const optionSetsIds = getLocalStorageOptionSetIDs();
+        const newOptionSetId = getUniqueRandomInt(optionSetsIds);
+        newOs.MenuItemOptionSetId = newOptionSetId;
+
+        newOs.MenuItemOptionSetItems.forEach(option => {
+            const optionIds = getLocalStorageOptionSetItemsIDs();
+            const newOptionId = getUniqueRandomInt(optionIds);
+            option.MenuItemOptionSetItemId = newOptionId
+        })
+
+        newOs.DisplayOrder = foundItem.MenuItemOptionSets.length
+
+        foundItem.MenuItemOptionSets.push(newOs)
+
+        const osContainerPreviewArray = Array.from(document.getElementsByClassName('osContainer'));
+        const osContainerPreview = osContainerPreviewArray.find((p) => p.id == foundItem.MenuItemId);
+
+        const newOptionRow = createOsRow(newOs, foundItem.MenuSectionId, foundItem.MenuItemId)
+        osContainerPreview.appendChild(newOptionRow);
+
+        const osRowHeadersPreview = Array.from(document.getElementsByClassName('osRowHeader'))
+
+        osRowHeadersPreview.forEach((osRowHeaderPreview, index) => {
+            if (index % 2 === 0) {
+                osRowHeaderPreview.classList.remove('even');
+                osRowHeaderPreview.classList.add('odd');
+            } else {
+                osRowHeaderPreview.classList.remove('odd');
+                osRowHeaderPreview.classList.add('even');
+            }
+        });
+
+        groupOptionSets()
+        updateLocalStorage()
+    })
+
+    return addBtn
 }
 
 export { createOsModalFooter };
