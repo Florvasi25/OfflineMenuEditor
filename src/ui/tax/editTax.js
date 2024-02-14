f92c66f1abcbdcf02ecac4749f88e9c7b1a9841a
florencia.vasiliauskas <florvasiliauskas@gmail.com>
2024-02-14 18:21:20 -0300

    Edit tax updates taxValue & UI

--- src/ui/tax/editTax.js
+++ src/ui/tax/editTax.js
@@ -7,6 +7,8 @@ import { showToolTip } from '../toolTip.js';
 
 import { slotManagerInstance } from '../mainContainer.js';
 
+import { applyTaxToAll } from './applyTaxToAll.js';
+
 function editTax(savedTaxContainer, taxRate, savedTaxName, savedTaxRate) {
     const editTaxContainer = document.createElement('div');
     editTaxContainer.className = 'editTaxContainer';
@@ -108,7 +110,50 @@ function updateTax(taxPercent, savedTaxContainer, editTaxContainer, savedTaxName
         const taxRateToUpdate = jsonData.TaxRates.find(tax => tax.TaxRateId == taxRate.TaxRateId);
         taxRateToUpdate.Rate = parseFloat(taxPercent.textContent);
         taxRateToUpdate.Name = taxName.textContent.trim();
-        
+
+        const taxRatePercent = taxRate.Rate / 100
+        const excludedCheckbox = document.querySelector('.taxTypeCheckboxes .excludedCheckbox');
+        const includedCheckbox = document.querySelector('.taxTypeCheckboxes .includedCheckbox');
+    
+        jsonData.MenuSections.forEach(section => {
+            section.MenuItems.forEach(item => {
+                if (taxRate.TaxRateId == item.TaxRateId) {
+                    item.TaxRateId = taxRate.TaxRateId
+                    if (excludedCheckbox.querySelector('input[type="checkbox"]').checked) {
+                        item.TaxValue = Number((item.Price * taxRatePercent).toFixed(2));
+                    } else if (includedCheckbox.querySelector('input[type="checkbox"]').checked) {
+                        item.TaxValue = Number(((item.Price * taxRatePercent) / (1 + taxRatePercent)).toFixed(2));
+                    }
+                }
+                item.MenuItemOptionSets.forEach(os => {
+                    os.MenuItemOptionSetItems.forEach(option => {
+                        if (taxRate.TaxRateId == option.TaxRateId) {
+                            option.TaxRateId = taxRate.TaxRateId
+                            if (excludedCheckbox.querySelector('input[type="checkbox"]').checked) {
+                                option.TaxValue = Number((option.Price * taxRatePercent).toFixed(2));
+                            } else if (includedCheckbox.querySelector('input[type="checkbox"]').checked) {
+                                option.TaxValue = Number(((option.Price * taxRatePercent) / (1 + taxRatePercent)).toFixed(2));
+                            }
+                        }
+                    })
+                })
+            })
+        })
+    
+        const itemTaxPreviewArray = Array.from(document.getElementsByClassName('itemTax'));
+        itemTaxPreviewArray.forEach(itemTaxPreview => {
+            if (itemTaxPreview && itemTaxPreview.id == taxRate.TaxRateId) {
+                itemTaxPreview.textContent = taxRate.Rate + '%'
+            }
+        })
+
+        const optionTaxPreviewArray = Array.from(document.getElementsByClassName('optionTaxPreview'));
+        optionTaxPreviewArray.forEach(optionTaxPreview => {
+            if (optionTaxPreview && optionTaxPreview.id == taxRate.TaxRateId) {
+                optionTaxPreview.textContent = taxRate.Rate + '%'
+            }
+        })
+
         updateLocalStorage(slotManagerInstance.currentSlot);
 
         savedTaxName.textContent = 'Tax Name: ' + taxName.textContent;
